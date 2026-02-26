976bcc0c117a4bf4bccee69f92c700089f8f5c15
Lautaro Soriano <lautaro.soriano@hotmail.com>
2026-02-25 22:12:39 -0300

    Overlay's de habilidades de los agentes, footer de páginas, reorganización de nav y página dev actualizada

--- js/agentes.js
+++ js/agentes.js
@@ -1,20 +1,36 @@
-let astraCard = document.querySelector("#astra");
-let astraCard_img = document.querySelector("#astra_img");
-let band = false;
+const cards = document.querySelectorAll(".card_agente");
 
-astraCard.addEventListener("click", e=>{
-  band != band;
-})
+cards.forEach((card, index) => {
 
-if(band){
-  astraCard_img.style.display = "none";
-}else{
-  astraCard_img.style.display = "block";
-}
+  const overlay = card.querySelector(".overlay-habilidades");
+
+  card.addEventListener("mouseover", () => {
+
+    const agente = hab_agentes[index];
+
+    overlay.innerHTML = `
+    <p><strong></strong> ${agente.descripcion}</p>
+      <p><strong>C:</strong> ${agente.c}</p>
+      <p><strong>Q:</strong> ${agente.q}</p>
+      <p><strong>E:</strong> ${agente.e}</p>
+      <p><strong>X:</strong> ${agente.x}</p>
+    `;
+
+    overlay.style.opacity = "1";
+
+  });
+
+  card.addEventListener("mouseout", () => {
+
+    overlay.style.opacity = "0";
+
+  });
+
+});
 
 const hab_agentes = [
 {
-  descripcion: "Controladora que manipula el campo de batalla con energía cósmica.",
+  descripcion: "Controladora ghanesa que manipula el campo de batalla con energía cósmica.",
   c: "Pozo Gravitacional",
   q: "Pulso Nova",
   e: "Nebula",
@@ -22,7 +38,7 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Iniciador que utiliza explosiones sísmicas para abrir paso.",
+  descripcion: "Iniciador sueco que utiliza explosiones sísmicas para abrir paso.",
   c: "Réplica",
   q: "Falla",
   e: "Línea de Falla",
@@ -30,7 +46,7 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Controlador estratégico con apoyo orbital.",
+  descripcion: "Controlador estadounidense estratégico con apoyo orbital.",
   c: "Baliza Estimulante",
   q: "Granada Incendiaria",
   e: "Cortina de Humo",
@@ -38,23 +54,36 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Centinela elegante con armamento personalizado.",
+  descripcion: "Centinela fracés elegante con armamento personalizado.",
   c: "Marca Registrada",
   q: "Cazador de Cabezas",
   e: "Rendezvous",
   x: "Tour de Force"
 },
-
 {
-  descripcion: "Centinela experto en vigilancia e información.",
+  descripcion: "Controladora escocesa que manipula energía vital y puede seguir usando habilidades incluso después de morir.",
+  c: "Subidón",
+  q: "Entrometerse",
+  e: "Ardid",
+  x: "Todavía no estoy muerta"
+},
+{
+  descripcion: "Centinela marroquino experto en vigilancia e información.",
   c: "Jaula Cibernética",
   q: "Cámara Espía",
   e: "Cable Trampa",
   x: "Hurto Neural"
 },
+{
+  descripcion: "Centinela noruega experta en tecnología de contención que controla zonas y bloquea avances enemigos con dispositivos de alta precisión.",
+  c: "Sensor sónico",
+  q: "Malla de barrera",
+  e: "Red gravitatoria",
+  x: "Aniquilación"
+},
 
 {
-  descripcion: "Iniciadora que caza a sus enemigos con pesadillas.",
+  descripcion: "Iniciadora turca que caza a sus enemigos con pesadillas.",
   c: "Acechadora",
   q: "Capturar",
   e: "Atormentar",
@@ -62,15 +91,15 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Iniciador que combate junto a sus criaturas.",
-  c: "Mosh Pit",
-  q: "Dizzy",
-  e: "Wingman",
-  x: "Thrash"
+  descripcion: "Iniciador estadounidense que combate junto a sus criaturas.",
+  c: "Pogo Brutal",
+  q: "Carnalito",
+  e: "Vértigo",
+  x: "Paliza"
 },
 
 {
-  descripcion: "Controlador que domina el agua para proteger y bloquear visión.",
+  descripcion: "Controlador indio que domina el agua para proteger y bloquear visión.",
   c: "Cascada",
   q: "Ensenada",
   e: "Marea Alta",
@@ -78,7 +107,7 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Duelista enfocado en duelos uno contra uno.",
+  descripcion: "Duelista chino enfocado en duelos uno contra uno.",
   c: "Contingencia",
   q: "Debilitamiento",
   e: "Doble Toque",
@@ -86,7 +115,7 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Duelista ágil experta en movilidad.",
+  descripcion: "Duelista coreana ágil experta en movilidad.",
   c: "Borrasca",
   q: "Vendaval",
   e: "Impulso",
@@ -102,7 +131,7 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Centinela que usa tecnología para controlar zonas.",
+  descripcion: "Centinela alemana que usa tecnología para controlar zonas.",
   c: "Nanoenjambre",
   q: "Bot de Alarma",
   e: "Torreta",
@@ -110,7 +139,7 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Duelista de velocidad eléctrica.",
+  descripcion: "Duelista filipina de velocidad eléctrica.",
   c: "Carril Rápido",
   q: "Relámpago",
   e: "Alta Velocidad",
@@ -118,7 +147,7 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Controlador que manipula sombras y teletransportación.",
+  descripcion: "Controlador de origen desconocido que manipula sombras y teletransportación.",
   c: "Paranoia",
   q: "Paso Cubierto",
   e: "Velo Tenebroso",
@@ -126,7 +155,7 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Duelista que controla el fuego para curarse y atacar.",
+  descripcion: "Duelista inglés que controla el fuego para curarse y atacar.",
   c: "Bola Curva",
   q: "Manos Calientes",
   e: "Llamarada",
@@ -134,7 +163,7 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Duelista explosiva especialista en daño en área.",
+  descripcion: "Duelista brasilera explosiva especialista en daño en área.",
   c: "Bot Explosivo",
   q: "Fardo Explosivo",
   e: "Bomba de Pintura",
@@ -142,7 +171,7 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Duelista que se fortalece al eliminar enemigos.",
+  descripcion: "Duelista mexicana que se fortalece al eliminar enemigos.",
   c: "Devorar",
   q: "Mirada Lasciva",
   e: "Despreciar",
@@ -150,7 +179,7 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Centinela que protege y cura a su equipo.",
+  descripcion: "Centinela japonesa que protege y cura a su equipo.",
   c: "Orbe Barrera",
   q: "Orbe de Ralentización",
   e: "Orbe Sanador",
@@ -158,7 +187,7 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Iniciadora que guía a su equipo con criaturas espirituales.",
+  descripcion: "Iniciadora australiana que guía a su equipo con criaturas espirituales.",
   c: "Rastreadora",
   q: "Luz Guía",
   e: "Reverdecer",
@@ -166,11 +195,25 @@ const hab_agentes = [
 },
 
 {
-  descripcion: "Iniciador experto en reconocimiento y rastreo.",
+  descripcion: "Iniciador ruso experto en reconocimiento y rastreo.",
   c: "Dron Búho",
   q: "Proyectil de Choque",
   e: "Flecha de Reconocimiento",
   x: "Furia del Cazador"
+},
+{
+  descripcion: "Iniciador táctico colombiano que utiliza tecnología militar avanzada para limpiar zonas y obligar a los enemigos a desplazarse.",
+  c: "Granada guiada",
+  q: "Descarga cegadora",
+  e: "Salva estratégica",
+  x: "Bombardeo táctico"
+},
+{
+  descripcion: "Centinela originario de Senegal que, con la ayuda de una mutación genética, desafía las reglas del combate al anular los poderes y la tecnología de sus oponentes. En el campo de batalla de Veto, las armas son tu única garantía.",
+  c: "Correa",
+  q:"Atajo",
+  e: "Interceptor",
+  x: "Evolución"
 },
 
 {
@@ -188,7 +231,13 @@ const hab_agentes = [
   e: "Muro Metálico",
   x: "Jardín de Espinas"
 },
-
+{
+  descripcion: "Duelista que manipula el tiempo para ralentizar a sus enemigos y moverse a gran velocidad, creando ventajas agresivas en los enfrentamientos.",
+  c: "Distorsión temporal",
+  q: "Pulso ralentizador",
+  e: "Desfase rápido",
+  x: "Fractura del tiempo"
+},
 {
   descripcion: "Duelista que manipula dimensiones y engaño.",
   c: "Engaño",
